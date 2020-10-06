0b407af724731750aecb2d92f2fda0ccc7b4bc71
ayslanmarcelino <ayslan_marcelino@outlook.com>
2020-10-06 01:30:57 -0300

    feature/add-user-crud-and-login-screen Add view users for admin
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :admins do
+    get 'users/index'
+
+    resources :users
+  end
+
   get 'dashboard/index'
   devise_for :users
 
