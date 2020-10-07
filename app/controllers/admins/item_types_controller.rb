e4ef1a89c38ca41b517750d15d1ae56a58f7f745
ayslanmarcelino <ayslan_marcelino@outlook.com>
2020-10-07 03:34:41 -0300

    feature/add-crud-item-types Add edit and delete item types
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>

--- app/controllers/admins/item_types_controller.rb
+++ app/controllers/admins/item_types_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 module Admins
-  class ItemTypesController < UsersController
+  class ItemTypesController < AdminsController
+    before_action :set_item_type, only: %w[edit update destroy]
+
     def index
       @item_types = ItemType.all.order(:description)
     end
@@ -16,7 +18,21 @@ module Admins
       @item_type.save ? (redirect_to admins_item_types_path, notice: 'Tipo de item cadastrado com sucesso') : (render :new)
     end
 
+    def edit; end
+
+    def update
+      @item_type.update(params_item_type) ? (redirect_to admins_item_types_path, notice: 'Tipo de item atualizado com sucesso') : (render :edit)
+    end
+
+    def destroy
+      @item_type.destroy ? (redirect_to admins_item_types_path, notice: 'Tipo de item excluído com sucesso') : (render :index)
+    end
+
     private
+
+    def set_item_type
+      @item_type = ItemType.find(params[:id])
+    end
 
     def params_item_type
       params.require(:item_type).permit(:description)
