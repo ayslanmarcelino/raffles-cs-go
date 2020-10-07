fa5371138c733009b2f54f7095e652feb159cef1
ayslanmarcelino <ayslan_marcelino@outlook.com>
2020-10-07 03:11:43 -0300

    feature/add-crud-item-types Add item types model and view without edit and delete
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>

---
+++ app/controllers/admins/item_types_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Admins
+  class ItemTypesController < UsersController
+    def index
+      @item_types = ItemType.all.order(:description)
+    end
+
+    def new
+      @item_type = ItemType.new
+    end
+
+    def create
+      @item_type = ItemType.new(params_item_type)
+
+      @item_type.save ? (redirect_to new_admins_item_type_path, notice: 'Tipo de item cadastrado com sucesso') : (render :new)
+    end
+
+    private
+
+    def params_item_type
+      params.require(:item_type).permit(:description)
+    end
+  end
+end
