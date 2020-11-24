2ab047efa831a4b445b34b92e8af26ed36fbc1aa
Ayslan Marcelino <43254034+ayslanmarcelino@users.noreply.github.com>
2020-11-23 23:42:08 -0300

    feature/add-available-skins-menu Add available skins (#57)
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>

---
+++ app/controllers/admins/available_skins_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Admins
+  class AvailableSkinsController < AdminsController
+    before_action :set_skin, only: %w[show edit update destroy]
+    helper_method :sort_column, :sort_direction
+
+    def index
+      @skins = Skin.all.order(sort_column + ' ' + sort_direction).where(is_available: true)
+    end
+
+    private
+
+    def sort_column
+      Skin.column_names.include?(params[:sort]) ? params[:sort] : 'created_at'
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : 'desc'
+    end
+  end
+end
+  
