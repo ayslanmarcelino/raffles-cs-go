961ee20ecd79678a56aab829b3f3fe29b4ff0e06
ayslanmarcelino <ayslan_marcelino@outlook.com>
2021-01-03 21:47:24 -0300

    feature/add-filters-in-skins-and-remove-available-skins-menu Add filters skins layout in market and skins
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>

--- app/controllers/admins/available_skins_controller.rb
+++ app/controllers/admins/available_skins_controller.rb
@@ -2,27 +2,15 @@
 
 module Admins
   class AvailableSkinsController < AdminsController
-    before_action :set_skin, only: %w[show edit update destroy]
-    helper_method :sort_column, :sort_direction
-
     def index
-      @skins = Skin.all
-                   .joins(:steam_account)
-                   .where("steam_accounts.user_id = #{current_user.id}")
-                   .order(sort_column + ' ' + sort_direction)
-                   .where(is_available: true)
-                   .where('expiration_date < ? OR expiration_date is null', DateTime.now)                   
-    end
-
-    private
-
-    def sort_column
-      Skin.column_names.include?(params[:sort]) ? params[:sort] : 'created_at'
-    end
-
-    def sort_direction
-      %w[asc desc].include?(params[:direction]) ? params[:direction] : 'desc'
+      @q = Skin.joins(:steam_account)
+               .where("steam_accounts.user_id = #{current_user.id}")
+               .where(is_available: true)
+               .where('expiration_date < ? OR expiration_date is null', DateTime.now)
+               .page(params[:page])
+               .ransack(params[:q])
+      @skins = @q.result(distinct: true)
+      @available_skins = Skin.where(is_available: true)
     end
   end
 end
-  
