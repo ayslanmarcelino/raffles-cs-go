7abeeda304b01eff2808b02f7fdee3424d9d7b17
ayslanmarcelino <ayslan_marcelino@outlook.com>
2021-01-28 23:59:53 -0300

    feature/add-enterprise Add separator market
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>

--- app/controllers/market_controller.rb
+++ app/controllers/market_controller.rb
@@ -6,7 +6,14 @@ class MarketController < UsersController
   private
 
   def skin_available_to_sale
+    steam_accounts = current_user.enterprise.user.map(&:steam_account)
+    @steam_account_ids = ''
+    steam_accounts.each do |steam_account|
+      @steam_account_ids << steam_account.ids.join(', ')
+    end
+
     @q = Skin.includes(:steam_account)
+             .where({ steam_account_id: @steam_account_ids })
              .where(is_available: true)
              .where('price_steam > price_paid OR price_csmoney > price_paid')
              .where('price_steam > sale_price OR price_csmoney > sale_price')
