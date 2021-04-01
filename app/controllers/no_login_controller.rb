8089a3d53e9518753dfa7607b1c0a85db27653fe
Ayslan Marcelino <43254034+ayslanmarcelino@users.noreply.github.com>
2021-04-01 00:57:01 -0300

    Feature/new market (#100)
    * feature/new-market Add first layout to new market
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Update gemfile
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Fix gemfile.lock
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Fix pagination
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Fix title page and pagination icons
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Update gemfile lock
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Fix layout and add translation
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Fix responsive menu
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Add online chat
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Update layout new market
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Add minimalist layout to skin cards
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Fix layout to smartphone
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Add new menu to new market
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Fix deploy
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Fix maps and fix layout errors
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Add others menu
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Add agents, graffiti, music kit and stickers to menu
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Fix redirects
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Add filters to skins
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Fix menus
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Fix order items on menu
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Fix progress bar float
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>
    
    * feature/new-market Update logged user view
    
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>

---
+++ app/controllers/no_login_controller.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class NoLoginController < ActionController::Base
+end
--- app/helpers/market_helper.rb
+++ app/helpers/market_helper.rb
@@ -25,4 +25,139 @@ module MarketHelper
         .sort
         .uniq
   end
+
+  def knifes_type_collection
+    Skin.pluck(:type_skin)
+        .uniq
+        .compact
+        .map! { |types| types['Knife'] }
+        .compact
+  end
+
+  def knifes_collection
+    @knifes = []
+    Skin.where(type_skin: knifes_type_collection).map(&:type_weapon).uniq.each do |skin|
+      @knifes << skin
+    end
+
+    @knifes.sort
+  end
+
+  def gloves_type_collection
+    Skin.pluck(:type_skin)
+        .uniq
+        .compact
+        .map! { |types| types['Gloves'] }
+        .compact
+  end
+
+  def gloves_collection
+    @gloves = []
+    Skin.where(type_skin: gloves_type_collection).map(&:description).uniq.each do |skin|
+      @gloves << skin
+    end
+
+    @gloves.sort
+  end
+
+  def rifles_type_collection
+    Skin.pluck(:type_skin)
+        .uniq
+        .compact
+        .map! { |types| [types['Rifle'], types['Sniper Rifle']] }
+        .flatten
+        .compact
+  end
+
+  def rifles_collection
+    @rifles = []
+    Skin.where(type_skin: rifles_type_collection).map(&:type_weapon).uniq.each do |skin|
+      @rifles << skin
+    end
+
+    @rifles.sort
+  end
+
+  def pistols_type_collection
+    Skin.pluck(:type_skin)
+        .uniq
+        .compact
+        .map! { |types| types['Pistol'] }
+        .compact
+  end
+
+  def pistols_collection
+    @pistols = []
+    Skin.where(type_skin: pistols_type_collection).map(&:type_weapon).uniq.each do |skin|
+      @pistols << skin
+    end
+
+    @pistols.sort
+  end
+
+  def shotguns_type_collection
+    Skin.pluck(:type_skin)
+        .uniq
+        .compact
+        .map! { |types| types['Shotgun'] }
+        .compact
+  end
+
+  def shotguns_collection
+    @shotguns = []
+    Skin.where(type_skin: shotguns_type_collection).map(&:type_weapon).uniq.each do |skin|
+      @shotguns << skin
+    end
+
+    @shotguns.sort
+  end
+
+  def smgs_type_collection
+    Skin.pluck(:type_skin)
+        .uniq
+        .compact
+        .map! { |types| types['SMG'] }
+        .compact
+  end
+
+  def smgs_collection
+    @smgs = []
+    Skin.where(type_skin: smgs_type_collection).map(&:type_weapon).uniq.each do |skin|
+      @smgs << skin
+    end
+
+    @smgs.sort
+  end
+
+  def stickers_type_collection
+    Skin.pluck(:type_skin)
+        .uniq
+        .compact
+        .map! { |types| types['Sticker'] }
+        .compact
+  end
+
+  def agents_type_collection
+    Skin.pluck(:type_skin)
+        .uniq
+        .compact
+        .map! { |types| types['Agent'] }
+        .compact
+  end
+
+  def music_kits_type_collection
+    Skin.pluck(:type_skin)
+        .uniq
+        .compact
+        .map! { |types| types['Music Kit'] }
+        .compact
+  end
+
+  def graffiti_type_collection
+    Skin.pluck(:type_skin)
+        .uniq
+        .compact
+        .map! { |types| types['Graffiti'] }
+        .compact
+  end
 end
