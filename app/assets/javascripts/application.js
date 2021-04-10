cf6b7900a4d994b8663b0a0b926af6a83648d580
ayslanmarcelino <ayslan_marcelino@outlook.com>
2021-04-10 18:31:31 -0300

    feature/fix-layouts-devise Update layout to admin menus
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,5 @@
 //= require rails-ujs
 //= require activestorage
-//= require jquery_ujs
 //= require bootstrap/dist/js/bootstrap
 //= require metismenu/dist/metisMenu
 //= require bootstrap-growl-ifightcrime/jquery.bootstrap-growl
