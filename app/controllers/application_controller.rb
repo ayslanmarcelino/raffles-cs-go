b2011c697f1dfa2a4d6fd9d1ca51bf7b64a0e190
ayslanmarcelino <ayslan_marcelino@outlook.com>
2021-04-10 16:07:20 -0300

    feature/fix-layouts-devise Update logged user menu
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :skin_available_to_sale, if: :devise_controller?
+  skip_before_action :verify_authenticity_token
 
   layout 'application'
 
