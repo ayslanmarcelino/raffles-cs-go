353dec229f5fa4dd808b4f5bed50502b78de97aa
ayslanmarcelino <ayslan_marcelino@outlook.com>
2021-06-12 14:18:40 -0300

    feature/fix-all-project Fix colors and skins count
    -
    
    Signed-off-by: ayslanmarcelino <ayslan_marcelino@outlook.com>

--- app/controllers/admins/skins_controller.rb
+++ app/controllers/admins/skins_controller.rb
@@ -12,8 +12,9 @@ module Admins
                .ransack(params[:q])
 
       @skins = @q.result(distinct: true)
-      @all_skins = Skin.all
-      @available_skins = Skin.where(is_available: true)
+      @all_skins = Skin.joins(:steam_account).where('steam_accounts.user_id' => current_user.id)
+      @available_skins = Skin.joins(:steam_account)
+                              .where('steam_accounts.user_id' => current_user.id, is_available: true)
       @steam_accounts = SteamAccount.where(user_id: current_user.id)
                                     .order(:description)
     end
